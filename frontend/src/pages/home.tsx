62981d9352a027f987df9a624f4077cc1b62446f
Caleb Erickson <caleberickson02@gmail.com>
2026-01-26 12:10:33 -0600

    Initial setup with documentation and basic landing page. Also added CI/CD through vercel. Some AI was used to write basic documentation but the code was all handwritten for the setup

---
+++ frontend/src/pages/home.tsx
@@ -0,0 +1,21 @@
+// Import dependencies
+
+// Home page
+const Home = () => {
+
+
+    return (
+        <div className="flex flex-col w-full h-full items-center justify-start p-4">
+            <h1 className="text-6xl mt-6 font-bold text-light-accentText dark:text-dark-accentText">
+                Strata OS
+            </h1>
+            <h3 className="text-4xl font-semibold text-light-text-secondary dark:text-dark-text-secondary">
+                A University of Alabama CS495 Project
+            </h3>
+        </div>
+    )
+
+};
+
+// Export home page for imports
+export default Home;
