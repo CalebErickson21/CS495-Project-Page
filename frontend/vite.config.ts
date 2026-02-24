b2ffde0e61bda465b9c9bb8ff14e5406b6f23164
Caleb Erickson <caleberickson02@gmail.com>
2026-02-24 13:14:43 -0600

    Update deliverables with sprint 1 and added dropdowns for each sprint as well as project goals. need to make deliverables card a reusable component

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,5 +12,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  assetsInclude: ['**/*.pptx'],
+  assetsInclude: ['**/*.pptx', '**/*.docx', '**/*.xlsx'],
 })
